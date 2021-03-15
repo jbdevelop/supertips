35aceaab2c5d8ef97afb94e77138b2288713b98d
Julio Bernardo <jbernardo@gmail.com>
2021-03-15 15:50:10 -0300

    refactor: eslint config

---
+++ src/routes.ts
@@ -0,0 +1,9 @@
+import { Router } from 'express'
+import { ServerResponse } from 'node:http';
+
+const routes = Router()
+
+
+// routes.get('/', (request, response): => {
+//   return response.json("OK");
+// });
