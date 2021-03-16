154bfc64aa65f9a5b05f3e8a22aea52f2ae792f0
Julio Bernardo <jbernardo@gmail.com>
2021-03-16 20:28:10 -0300

    feature: add Tip Repository

---
+++ src/repositories/TipRepository.ts
@@ -0,0 +1,29 @@
+/* eslint-disable @typescript-eslint/ban-types */
+import TipSchema from '../schemas/Tip'
+import TipInterface from '../interfaces/Tip'
+
+
+import { Request } from 'express'
+
+class TipRepository {
+  public async index (): Promise<TipInterface[]> {    
+    const tips = await TipSchema.find()
+    
+    return tips
+  }  
+
+  public async create (tip: Request): Promise<TipInterface> {
+    const createdTip = await TipSchema.create(tip)    
+
+    return createdTip
+  }
+  
+  public async show (tags: Array<string>): Promise<TipInterface[]> {
+    const tips = await TipSchema.find({ tags: { $in: tags } })
+
+    return tips
+  }
+
+}
+
+export default new TipRepository()
