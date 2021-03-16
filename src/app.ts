e6750637de4f4228c493a56dd7de6357790144a0
Julio Bernardo <jbernardo@gmail.com>
2021-03-16 20:24:55 -0300

    chore: add dotenv

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+import * as dotenv from 'dotenv'
 import express from 'express'
 import cors from 'cors'
 import mongoose from 'mongoose'
@@ -8,6 +9,8 @@ class App {
   public express: express.Application
 
   constructor () {
+    dotenv.config()
+    
     this.express = express()
 
     this.middlewares()
@@ -20,8 +23,8 @@ class App {
     this.express.use(cors())
   }
 
-  private database () {  
-    const uri = 'mongodb+srv://admin:rRimjy6F8c1EYYl4@cluster0.kwono.mongodb.net/supertips?retryWrites=true&w=majority'  
+  private database () {      
+    const uri = `mongodb+srv://${process.env.DATABASE_USERNAME}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_HOST}/${process.env.DATABASE_NAME}?${process.env.DATABASE_URL_PARAMS}`  
     mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
   }
 
