23a47928fb9e099f2db845ff4d8ee5c586fad337
Julio Bernardo <jbernardo@gmail.com>
2021-03-16 20:28:45 -0300

    feature: add Tip Service

---
+++ src/services/TipService.ts
@@ -0,0 +1,31 @@
+/* eslint-disable @typescript-eslint/ban-types */
+import { Request } from 'express'
+import TipRepository from '../repositories/TipRepository'
+import TipInterface from '../interfaces/Tip'
+
+
+class TipService {
+  public async index (): Promise<TipInterface[]> {    
+    const tips = await TipRepository.index()    
+    
+    return tips
+  }  
+
+  public async create (tip: Request): Promise<TipInterface> {    
+    const createdTip = await TipRepository.create(tip)
+
+    return createdTip
+  }
+
+  public async show (tags: string): Promise<TipInterface[]> {    
+    const tagsArray = tags.split(',')
+
+    const tips = await TipRepository.show(tagsArray)    
+
+    const tipsFiltered = tips.filter(tip => tagsArray.every(ta => tip.tags.includes(ta))) 
+
+    return tipsFiltered
+  }
+}
+
+export default new TipService()
