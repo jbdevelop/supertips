290016fa04f524ceaf6c765744286854dad68994
Julio Bernardo <jbernardo@gmail.com>
2021-03-16 20:30:14 -0300

    refactor: Tip Controller calling Tip Service

--- src/controllers/TipController.ts
+++ src/controllers/TipController.ts
@@ -1,12 +1,27 @@
 import { Request, Response } from 'express'
 
-import Tip from '../schemas/Tip'
+import TipRepository from '../repositories/TipRepository'
+import TipService from '../services/TipService'
 
 class TipController {
   public async index (request: Request, response: Response): Promise<Response> {    
-    const tips = await Tip.find()
+    const tips = await TipService.index()
+    
+    return response.status(200).json(tips)        
+  }
 
-    return response.json(tips)    
+  public async create (request: Request, response: Response): Promise<Response> {
+    const tip = await TipRepository.create(request.body)        
+
+    return response.status(201).json(tip)
+  }
+
+  public async show (request: Request, response: Response): Promise<Response>{
+    const { tags } = request.params
+
+    const tips = await TipService.show(tags)    
+
+    return response.status(200).json(tips)
   }
 }
 
